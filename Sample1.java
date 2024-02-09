f07d817264c15da409f71042b4183216f4e6e224
sairaghu1030 <130482719+sairaghu1030@users.noreply.github.com>
2024-02-09 14:55:12 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,18 @@
+public class Sample1 {
+    int a=1;
+    int b=7;
+    Sample1(int a,int b)
+    {
+        a=5;
+        b=10;
+    }
+    void add(int a,int b) {
+        int c = a + b;
+        System.out.println("Sum: " + c);
+    }
+//
+    Sample1()
+    {
+
+    }
+}
---
+++ Sample2.java
@@ -0,0 +1,8 @@
+public class Sample2 extends Sample1{
+    public static void main(String[] args)
+    {
+        Sample2 S=new Sample2();
+        S.add(30,45);
+    }
+}
+
