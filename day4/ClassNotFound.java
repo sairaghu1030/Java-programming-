f2f175e817e5b09410aefed062059cae561a77fa
sairaghu1030 <130482719+sairaghu1030@users.noreply.github.com>
2024-02-12 14:34:25 +0530

    Update ClassNotFound.java

--- day4/ClassNotFound.java
+++ day4/ClassNotFound.java
@@ -1,7 +1,7 @@
 public class ClassNotFound {
     public static void main(String[] args) {
         try {
-            Class.forName("Java0927");
+            Class.forName("Java0710");
         } catch (ClassNotFoundException e) {
             System.out.println("An error occurred.\n"+e);
         }
